105da6e6bc7b47dea6c1b1d0664b92352d3b9e78
matheus <matheusdev20@gmail.com>
2025-01-18 18:44:13 -0300

    fix: removing @types from the app router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,8 +36,6 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const locale = await getLocale();
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
   return (
     <html lang={locale} className="scroll-smooth scroll-pt-[92px]">
--- src/app/utils/json.ts
+++ src/app/utils/json.ts
@@ -1,7 +1,7 @@
  
 import socials from "@/app/config/socials.json"
 import bio from "@/app/config/bio.json"
-import { Socials, Bio } from "../@types"
+import { Socials, Bio } from "../../@types"
 
 export const loadSocialConfigs = (): Socials => {
   return socials as unknown as Socials
