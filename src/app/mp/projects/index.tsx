64dff5934a85667858dc8dcb80c47f4eb00fb3b3
matheus <matheusdev20@gmail.com>
2025-01-19 21:29:10 -0300

    feat: internazinalization projets page

--- src/app/mp/projects/index.tsx
+++ src/app/mp/projects/index.tsx
@@ -3,7 +3,6 @@ import { useTranslations } from "next-intl"
 export const Projects = () => {
 
   const t = useTranslations("Project")
-
   return (
     <div id="projects" className="min-h-[calc(100vh-92px)] flex flex-col debug md:gap-8 items-center md:p-4">
       <header className="max-w-[50%] flex flex-col gap-2">
