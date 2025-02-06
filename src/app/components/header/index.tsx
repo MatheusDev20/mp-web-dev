214cb3169d8f7567a9298498f7ed7b328fe42a70
matheus <matheusdev20@gmail.com>
2025-02-06 07:24:13 -0300

    fix: some unseen lint files

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,26 +1,28 @@
-"use client"
-import MP from "../../../../public/mp-dark.png"
-import BR from "../../../../public/brazil.svg"
-import US from "../../../../public/usa.svg"
+"use client";
+import MP from "../../../../public/mp-dark.png";
+import BR from "../../../../public/brazil.svg";
+import US from "../../../../public/usa.svg";
 
-import Image from "next/image"
-import React from "react"
-import { CountrySelect } from "../language-select"
-import Link from "next/link"
-import RandomLetterSwapForward from "../fancy/letter-swap-hover"
-import { SocialNav } from "./nav/social"
-import { Menu } from "./nav/menu"
-import { useLocale} from "next-intl"
+import Image from "next/image";
+import React from "react";
+import { CountrySelect } from "../language-select";
+import Link from "next/link";
+import RandomLetterSwapForward from "../fancy/letter-swap-hover";
+import { SocialNav } from "./nav/social";
+import { Menu } from "./nav/menu";
+import { useLocale } from "next-intl";
 
 type Props = {
-  navHover: { socials: boolean, menu: boolean }
-  setNavHover: (hoverOptions: { socials: boolean, menu: boolean }) => void
-}
+  navHover: { socials: boolean; menu: boolean };
+  setNavHover: (hoverOptions: { socials: boolean; menu: boolean }) => void;
+};
 
 export const Header = ({ navHover, setNavHover }: Props) => {
-
   const locale = useLocale();
-  const options = [{name: "PT", imgLink: BR } ,{ name: "EN", imgLink: US}]
+  const options = [
+    { name: "PT", imgLink: BR },
+    { name: "EN", imgLink: US },
+  ];
 
   return (
     <header className="flex justify-center md:h-[92px] w-full py-0 px-0 md:px-16 border-b border-gray-700/50">
@@ -48,13 +50,9 @@ export const Header = ({ navHover, setNavHover }: Props) => {
         {/* Icons and Language Selector */}
         <div className="flex flex-row items-center mr-6 md:mr-0 gap-4 md:gap-8 md:mt-0">
           {/* Country Select */}
-          <CountrySelect
-            value={locale}
-            options={options}
-          />
+          <CountrySelect value={locale} options={options} />
         </div>
       </div>
     </header>
-
-  )
-}
+  );
+};
